f1bc24e791d3cf05725b575d3ff04cf6bbc80942
Skyler Knezevic <micarknez@gmail.com>
2019-06-30 17:55:29 -0400

    test-insurance

---
+++ animal-kingdom-master/src/constants.js
@@ -0,0 +1,81 @@
+import { AppConfig } from 'blockstack'
+
+export const appConfig = new AppConfig(['store_write', 'publish_data'])
+export const ANIMALS = [
+    {
+    id: 'insurance1',
+    name: 'Insurance 1',
+    superpower: 'Money'
+  },
+  {
+    id: 'insurance2',
+    name: 'Insurance 2',
+    superpower: 'Money'
+  },
+  {
+    id: 'insurance3',
+    name: 'Insurance 3',
+    superpower: 'Money'
+  },
+  {
+    id: 'insurance4',
+    name: 'Insurance 4',
+    superpower: 'Money'
+  }
+  ]
+
+export const TERRITORIES = [
+  {
+    id: 'hospital1',
+    name: 'Hospital 1',
+    superpower: 'Doctors'
+  },
+  {
+    id: 'hospital2',
+    name: 'Hospital 2',
+    superpower: 'Doctors'
+  },
+  {
+    id: 'hospital3',
+    name: 'Hospital 3',
+    superpower: 'Doctors'
+  },
+  {
+    id: 'hospital4',
+    name: 'Hospital 4',
+    superpower: 'Doctors'
+  }
+]
+
+export const ME_FILENAME = 'me.json'
+export const SUBJECTS_FILENAME = 'subjects.json'
+export const EXPLORER_URL = 'https://explorer.blockstack.org'
+
+
+export const OTHER_KINGDOMS = [
+  {
+    app: 'https://animal-kingdom-1.firebaseapp.com',
+    ruler: 'larry.id'
+  },
+  {
+    app: 'http://localhost:3001',
+    ruler: 'larz.id'
+  },
+  {
+    app: 'https://decentralised-islands.netlify.com',
+    ruler: 'yannael_leborgne.id'
+  },
+  {
+      app: 'https://thirsty-jang-0c0a17.netlify.com',
+      ruler: 'ma1222042.id.blockstack'
+  },
+  {
+    app: 'https://animalkingdoms.netlify.com',
+    ruler: 'moxiegirl.id.blockstack'
+  },
+  {
+    app: 'http://localhost:3000',
+    ruler: 'skylerknez.id.blockstack'
+  }
+ 
+]
